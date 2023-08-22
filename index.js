a35abf1d41d951d11706aaef3ad721e73e097d17
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-08-22 10:26:34 -0700

    I was able to get the objects to print out, but I am not sure that it is right. I can't get the prettyPrint function to work. I just want to see that it's working. the .sort for array's is not sorting the way I thought it would.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function Node(data, leftChild = null, rightChild = null) {
+function TreeNode(data, leftChild = null, rightChild = null) {
   return {
     data,
     leftChild,
@@ -19,15 +19,32 @@ function buildTree(array) {
   array.sort();
   //remove duplicates
 
-  if (array < 2) return Node(array);
+  if (array.length < 2) return TreeNode(array);
 
   const mid = Math.floor(array.length / 2);
   let left = array.slice(0, mid);
   let right = array.slice(mid);
 
-  let node = Node(mid);
-  node.leftChild = buildTree(left);
-  node.rightChild = buildTree(right);
+  let parent = TreeNode(array[mid]);
+  parent.leftChild = buildTree(left);
+  parent.rightChild = buildTree(right);
 
-  return node;
+  return parent;
 }
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+console.log(JSON.stringify(buildTree(testArray)));
