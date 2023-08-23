b3497347a52afbff808c1c279654a75ecaedfc7b
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-08-22 17:39:08 -0700

    I wrote a for loop for get rid of duplicates from the array

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-function TreeNode(data, left = null, right = null) {
-  return {
-    data,
-    left,
-    right,
-  };
-}
-
 function Tree(array) {
+  array.sort(function (a, b) {
+    return a - b;
+  });
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[i + 1]) {
+      array.splice(i, 1);
+    } else {
+    }
+  }
+
   const root = buildTree(array);
-  const getRoot = () => root;
   // rebalance tree
+
+  function TreeNode(data, left = null, right = null) {
+    return {
+      data,
+      left,
+      right,
+    };
+  }
+  // willow amalia larsen
+
+  // daddy larsen
+  // mommy larsen
+
+  function buildTree(array) {
+    //remove duplicates
+
+    if (array.length < 2) return TreeNode(array[0]);
+
+    const mid = Math.floor(array.length / 2);
+    let left = array.slice(0, mid);
+    let right = array.slice(mid + 1);
+
+    let parent = TreeNode(array[mid]);
+    parent.left = buildTree(left);
+    parent.right = buildTree(right);
+
+    return parent;
+  }
   return {
-    getRoot,
+    root: () => root,
   };
-}
-
-function buildTree(array) {
-  //remove duplicates
-
-  if (array.length < 2) return TreeNode(array[0]);
-
-  const mid = Math.floor(array.length / 2);
-  let left = array.slice(0, mid);
-  let right = array.slice(mid + 1);
-
-  let parent = TreeNode(array[mid]);
-  parent.left = buildTree(left);
-  parent.right = buildTree(right);
-
-  return parent;
 }
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
@@ -46,8 +60,5 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 
 let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 2, 78];
 
-testArray.sort(function (a, b) {
-  return a - b;
-});
-
-console.log(prettyPrint(buildTree(testArray)));
+const tree = Tree(testArray);
+console.log(prettyPrint(tree.root()));
