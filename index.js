052e6b808c6749da9cd3ea0826f873fc7aa078ed
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-08-23 18:57:45 -0700

    had ben help me with insert. I can read what he changed, but it was mainly what I wrote. He also reorginzed it.

--- index.js
+++ index.js
@@ -7,14 +7,59 @@ function Tree(array) {
   const root = buildTree(sortedArray);
 
   function TreeNode(data, left = null, right = null) {
+    function insert(val) {
+      if (val < data) {
+        if (left === null) {
+          left = TreeNode(val);
+        } else {
+          left.insert(val);
+        }
+      } else {
+        if (right === null) {
+          right = TreeNode(val);
+        } else {
+          right.insert(val);
+        }
+      }
+    }
+
+    const prettyPrint = (prefix = '', isLeft, isRoot = false) => {
+      if (right !== null) {
+        right.prettyPrint(
+          `${prefix}${isLeft && !isRoot ? '│   ' : '    '}`,
+          false
+        );
+      }
+      if (isRoot) {
+        console.log(`${data}`);
+      } else {
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${data}`);
+      }
+      if (left !== null) {
+        left.prettyPrint(
+          `${prefix}${isLeft || isRoot ? '    ' : '│   '}`,
+          true
+        );
+      }
+    };
+
     return {
       data,
-      left,
-      right,
+      getLeft: () => left,
+      setLeft: (node) => {
+        left = node;
+      },
+      getRight: () => right,
+      setRight: (node) => {
+        right = node;
+      },
+      insert,
+      prettyPrint,
     };
   }
 
   function buildTree(array) {
+    if (array.length < 1) return null;
     if (array.length < 2) return TreeNode(array[0]);
 
     const mid = Math.floor(array.length / 2);
@@ -22,52 +67,38 @@ function Tree(array) {
     let right = array.slice(mid + 1);
 
     let parent = TreeNode(array[mid]);
-    parent.left = buildTree(left);
-    parent.right = buildTree(right);
+    parent.setLeft(buildTree(left));
+    parent.setRight(buildTree(right));
 
     return parent;
   }
+
+  const insert = (val) => {
+    if (root === null) {
+      root = TreeNode(val);
+    } else {
+      root.insert(val);
+    }
+  };
+
+  const prettyPrint = () => {
+    root?.prettyPrint('', true, true);
+  };
 
   //insert - case is to insert leaf.
   //if x < node move left
   //else move right
 
-  function insert(root, val) {
-    if (root === null) {
-      root = TreeNode(val);
-      return root;
-    }
-
-    if (val < root.data) {
-      root.left = insert(root.left, val);
-    } else {
-      root.right = insert(root.right, val);
-    }
-
-    return root;
-  }
-
   return {
-    root: () => root,
     insert,
+    prettyPrint,
   };
 }
-
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-  if (node === null) {
-    return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  }
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-  }
-};
 
 let testArray = [7, 55, 88, 22, 9, 5, 7, 9, 67, 6345, 324, 78];
 
 const tree = Tree(testArray);
-Tree.insert(60);
-console.log(prettyPrint(tree.root()));
+tree.insert(1);
+tree.insert(2);
+tree.insert(3);
+console.log(tree.prettyPrint());
