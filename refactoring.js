8bd4f715404a3b80c0452e3e3fd32012acea89e5
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-09-20 12:11:22 -0700

    ben told me to start over...

--- refactoring.js
+++ refactoring.js
@@ -11,62 +11,7 @@ function Tree(array) {
       this.right = null;
     }
   }
-
-  function buildTree(arr) {
-    if (arr.length < 1) return null;
-    if (arr.length < 2) return new Node(arr[0]);
-
-    let mid = Math.floor(arr.length / 2);
-    let leftArr = arr.slice(0, mid);
-    let rightArr = arr.slice(mid + 1);
-
-    let parent = new Node(arr[mid]);
-
-    parent.left = buildTree(leftArr);
-    parent.right = buildTree(rightArr);
-
-    return parent;
-  }
-
-  let root = buildTree(sortedArray);
-
-  const prettyPrint = (node, prefix = '', isLeft = true) => {
-    if (node === null) {
-      return;
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-    }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-    }
-  };
-
-  function insert(val, node = root) {
-    if (node == null) {
-      node == new Node(val);
-      return node;
-    }
-    if (val < node.data) {
-      node.left = insert(val, node.left);
-    } else if (val > node.data) {
-      node.right = insert(val, node.right);
-    }
-  }
-
-  //   console.log(JSON.stringify(theTree));
-  //   console.log(prettyPrint(root));
-
-  return {
-    prettyPrint,
-    insert,
-  };
 }
 
 let bigArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 let smallArray = [2, 4, 6];
-
-let theTree = Tree(bigArray);
-theTree.prettyPrint();
-// theTree.insert(6);
