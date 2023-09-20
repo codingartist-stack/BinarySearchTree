9c38f7024705e834f7e4df8d2bc14d84cc28d4a9
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-09-20 12:18:56 -0700

    refeactoring again

--- refactoring.js
+++ refactoring.js
@@ -11,6 +11,29 @@ function Tree(array) {
       this.right = null;
     }
   }
+
+  function buildTree(arr) {
+    if (arr.length === 0) {
+      return null;
+    }
+  }
+
+  const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  };
+
+  return {
+    prettyPrint,
+  };
 }
 
 let bigArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
